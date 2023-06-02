0ac07ff4dde10e5ac8f407cf8083ba43d5e746e7
jskroberta <129328635+jskroberta@users.noreply.github.com>
2023-06-02 17:30:17 -0300

    Atualização JavaScript

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function escolhaPokemon(nome_pokemon) {
     alert("Boa ESCOLHA! "+nome_pokemon);
     
 }
+
+function tocaSomPikachu() {
+    document.querySelector('#som_pikachu').play();  
+}
+document.querySelector('#pokePika').onclick = tocaSomPikachu;
